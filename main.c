6dcfaea08b4b00a8b62620a3248471fb7eadb84b
MarkymarkLee <b10902032@csie.ntu.edu.tw>
2021-11-26 00:00:41 +0800

    regular update from homepc

--- main.c
+++ main.c
@@ -2,7 +2,15 @@
 
 int main(){
 
-    printf("hello\n");
+    while(1){
+        static int  i = 10;
+        i++;
+        printf("%d\n",i);
+        if(i>20){
+            break;
+        }
+    }
+    printf("%d\n",i);
     
     return 0;
 }
